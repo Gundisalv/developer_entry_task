11ce6b6bf5a4399d5e06d136a36f12d044f9c8a1
gonza <gonzalo@optiagro.com>
2019-04-03 00:10:48 -0300

    initial files for module

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+if os.path.exists('config.env'):
+    print('Importing environment from .env file')
+    for line in open('config.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1].replace("\"", "")
+
+
+class Config:
+    SENTINEL_INSTANCE_ID = os.environ.get('SENTINEL_HUB_ID') or False
+    if not SENTINEL_INSTANCE_ID:
+        print('SENTINEL_HUB_ID needed to continue')
+        exit(0)
