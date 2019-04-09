944178dfa2bcae06ad9d01774523e6173c1040a4
gonza <gonzalo@optiagro.com>
2019-04-08 23:24:00 -0300

    final code

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ from developer_entry_task.functions.functions import DeveloperEntryTask
 
 Developer_Entry_Task = DeveloperEntryTask()
 
-Developer_Entry_Task.get_statistics(42)
-
-Developer_Entry_Task.get_statistics('forest')
-
-Developer_Entry_Task.get_closest_pair([2 , 3 , 5 , 7 , 11],'min')
-
-Developer_Entry_Task.get_closest_pair(list(range(10**4)),'mean', 'std')
+result = Developer_Entry_Task.get_statistics(42)
+print(result)
+result = Developer_Entry_Task.get_statistics('forest')
+print(result)
+result = Developer_Entry_Task.get_closest_pair([2 , 3 , 5 , 7 , 11],'min')
+print(result)
+result = Developer_Entry_Task.get_closest_pair(list(range(10**4)),'mean', 'std')
+print(result)
